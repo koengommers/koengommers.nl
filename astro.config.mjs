0f239fc22166c7768cdddd6f32222971fd2f66ba
Koen Gommers <koen@koengommers.nl>
2023-05-14 16:11:39 +0200

    feat: use poimandres theme for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: "https://koengommers.nl",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: "poimandres"
+    }
+  }
 });
